45a5e98b6c47ab0132f6d5b427b5e1cf866ddbc4
ryry013 <ryry013@yahoo.com>
2018-09-06 21:31:08 +0900

    Various formatting changes to Rai.py Added a command to save the database to owner.py Changed format of gcloud bat files

--- gcloud_download.bat
+++ gcloud_download.bat
@@ -3,4 +3,5 @@ call gcloud compute scp ryry013@centos:/home/ryry013/bot/cogs/owner.py ./cogs/ow
 call gcloud compute scp ryry013@centos:/home/ryry013/bot/cogs/main.py ./cogs/main.py
 call gcloud compute scp ryry013@centos:/home/ryry013/bot/cogs/welcome.py ./cogs/welcome.py
 call gcloud compute scp ryry013@centos:/home/ryry013/bot/cogs/math.py ./cogs/math.py
+call gcloud compute scp ryry013@centos:/home/ryry013/bot/database.json database.json
 pause
