7c00a95aa45033a0573f2856a7490d9ca308067e
ryry013 <ryry013@yahoo.com>
2018-11-17 12:09:13 +0900

    little bug fixes

--- gcloud_download.bat
+++ gcloud_download.bat
@@ -1,7 +1,7 @@
-call gcloud compute scp ryry013@centos:/home/ryry013/bot/Rai.py Rai.py 
-call gcloud compute scp ryry013@centos:/home/ryry013/bot/cogs/owner.py ./cogs/owner.py
-call gcloud compute scp ryry013@centos:/home/ryry013/bot/cogs/main.py ./cogs/main.py
-call gcloud compute scp ryry013@centos:/home/ryry013/bot/cogs/welcome.py ./cogs/welcome.py
-call gcloud compute scp ryry013@centos:/home/ryry013/bot/cogs/math.py ./cogs/math.py
-call gcloud compute scp ryry013@centos:/home/ryry013/bot/database.json database.json
+call gcloud compute scp ryry013@centos:/home/ryry013/Rai/Rai.py Rai.py 
+call gcloud compute scp ryry013@centos:/home/ryry013/Rai/cogs/owner.py ./cogs/owner.py
+call gcloud compute scp ryry013@centos:/home/ryry013/Rai/cogs/main.py ./cogs/main.py
+call gcloud compute scp ryry013@centos:/home/ryry013/Rai/cogs/welcome.py ./cogs/welcome.py
+call gcloud compute scp ryry013@centos:/home/ryry013/Rai/cogs/math.py ./cogs/math.py
+call gcloud compute scp ryry013@centos:/home/ryry013/Rai/database.json database.json
 pause
