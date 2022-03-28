51855e7a1b566e34b298a4e54549f4c74dc5fdbf
Ryan H <30587035+ryry013@users.noreply.github.com>
2022-03-28 07:45:06 -0700

    Oops DMChannel —> dm_channel

--- cogs/utils/helper_functions.py
+++ cogs/utils/helper_functions.py
@@ -147,8 +147,8 @@ async def safe_send(destination, content=None, *, wait=False, embed=None, delete
             return
 
     try:
-        if isinstance(destination, discord.Member):
-            if not destination.DMChannel:
+        if isinstance(destination, discord.User):
+            if not destination.dm_channel:
                 await destination.create_dm()
         return await destination.send(content, embed=embed, delete_after=delete_after, file=file)
     except discord.Forbidden:
