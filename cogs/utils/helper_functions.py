4e082bd62a20344ca86c2ed894c11d53a5bbd697
Ryan H <30587035+r-hensley@users.noreply.github.com>
2025-03-08 21:03:49 -0800

    Equality and reload in MiniMessage

--- cogs/utils/helper_functions.py
+++ cogs/utils/helper_functions.py
@@ -1351,7 +1351,13 @@ class MiniMessage:
             guild_id=message.guild_id,
             attachments=message.attachments,
         )
-    
+
+    def __eq__(self, other):
+        # noinspection PyUnresolvedReferences
+        if not isinstance(other, (MiniMessage, RaiMessage, discord.mixins.EqualityComparable)):
+            return False
+        return self.message_id == other.id
+
     def __repr__(self):
         """A clean string representation for debugging."""
         if len(self.content) > 20:
@@ -1402,6 +1408,7 @@ class MessageQueue(deque[MiniMessage]):
         to_dict_list: Convert all messages in the queue to a list of dictionaries.
         find_by_author: Find all messages by a specific author.
         change_length: Change the maximum length of the queue by creating a new queue and returning
+        reload: Reload all messages in queue with updated MiniMessage objects (assuming MiniMessage has changed)
     """
     def __init__(self, iterable: Iterable = (), maxlen: Optional[int] = None):
         """
@@ -1469,6 +1476,10 @@ class MessageQueue(deque[MiniMessage]):
     def average_message_length(self) -> float:
         """Calculate the average message length of the queue."""
         return sum(len(msg.content) for msg in self) / len(self) if len(self) else 0
+
+    def reload(self) -> None:
+        """Reload all messages in queue with updated MiniMessage objects (assuming MiniMessage has changed)"""
+        self.extend([MiniMessage.from_mini_message(msg) for msg in self])
 
     def add_message(self, message: Union[MiniMessage, discord.Message]) -> None:
         """Add a MiniMessage to the queue."""
